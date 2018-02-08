2b20c6e55dd157ec2a2b6bc017d34c77a2f71169
JBSchami <jbeda091@uottawa.ca>
2018-02-07 20:41:57 -0500

    Added tail recursion factorial method implementation, required for D1 and initial prototype

--- src/Eternity/Eternity.java
+++ src/Eternity/Eternity.java
@@ -2,6 +2,26 @@ package Eternity;
 
 public class Eternity {
 
+    public static int eFactorial(int x){
+        int runningTotal = 1;
+        if (x==0){
+            return runningTotal;
+        }
+        else{
+            return eFactorial(x-1, runningTotal*x);
+        }
+    }
+
+    private static int eFactorial(int x, int runningTotal){
+        if (x==0){
+            return runningTotal;
+        }
+        else{
+            return eFactorial(x-1, runningTotal*x);
+        }
+    }
+
+    /*
     //To be developed by Brandon Handfield
     public static double eExpEuler(double x){
 
@@ -26,8 +46,10 @@ public class Eternity {
     public static double eExpY(double x, double y){
 
     }
-    
+    */
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(eFactorial(10));
+        System.out.println(eFactorial(3));
     }
 }
