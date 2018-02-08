b94d99cbfbc2e00b7d079a14e14bc4ae0afc1fbc
JBSchami <jbeda091@uottawa.ca>
2018-02-07 20:32:13 -0500

    Added basic core fun skeleton

---
+++ src/Eternity/Eternity.java
@@ -0,0 +1,33 @@
+package Eternity;
+
+public class Eternity {
+
+    //To be developed by Brandon Handfield
+    public static double eExpEuler(double x){
+
+    }
+
+    //To be developed by Daniel Witkowski
+    public static double eLog(double x){
+
+    }
+
+    //To be developed by Edip Tac
+    public static double eBaseTenExp(double x){
+
+    }
+
+    //To be developed by Jonathan Bedard Schami
+    public static double eCos(double x){
+
+    }
+
+    //To be developed by Julien Fagnan
+    public static double eExpY(double x, double y){
+
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
