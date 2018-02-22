5b205af1add8e312f4967a18a6f3c8fb7bdd7d00
JBSchami <jbeda091@uottawa.ca>
2018-02-22 09:10:22 -0500

    Updated to cos to handle all 4 quadrants as well as zero angle values

--- src/Eternity/Eternity.java
+++ src/Eternity/Eternity.java
@@ -1,5 +1,7 @@
 package Eternity;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class Eternity {
     /**
      * Factorial is required for many of the other functions
@@ -93,19 +95,48 @@ public class Eternity {
      * @return the value to the precision determined.
      */
     public static double eCos(double x, double precision){
+        boolean isLeftSideQuadrant = false;
+        //from 90 to 180 cos(x) = -cos(pi-x)
+        if(x > 1.5708 && x <= 3.14159){
+            x = 3.14159-x;
+            isLeftSideQuadrant = true;
+        }
+        boolean isThirdQuadrant = false;
+        //from 180 to 270 cos(x) = -cos((-1)*(pi-x))
+        if(x > 3.14159 && x <= 4.71239){
+            isLeftSideQuadrant = true;
+            x = (-1)*(3.14159-x);
+        }
+
         double temp;
         double retVal = 0;
         double delta;
         int n = 0;
         do{
             temp = retVal;
+            //Cosine taylor series expansion
             retVal = temp + (eExpY((-1), n)*eExpY(x, 2*n))/eFactorial(2*n);
             n++;
+            //When retVal gets extremely small, it is zero
+
+
+            //Keep expanding the taylor series until desired precision is met
             if (retVal-temp < 0)
                 delta = (-1)*(retVal-temp);
             else
                 delta = retVal-temp;
+
         }while((delta) > precision);
+
+        if(isLeftSideQuadrant){
+            retVal = retVal*(-1);
+        }
+
+        if(retVal < 0 && (-1)*retVal < 1e-5)
+            retVal = 0;
+        if(retVal >= 0 && retVal > 1e-10)
+            retVal = 0;
+
         return retVal;
     }
 
@@ -130,6 +161,11 @@ public class Eternity {
     */
 
     public static void main(String[] args) {
-        
+        //System.out.println(eCos(0, 0.0001));
+        System.out.println(eCos(1.5708, 0.0001));
+        System.out.println(eCos(3.14159, 0.0001));
+        System.out.println(eCos(2.7, 0.0001));
+        System.out.println(eCos(4.71239, 0.0001));
+        System.out.println(eCos(3.2, 0.0001));
     }
 }
